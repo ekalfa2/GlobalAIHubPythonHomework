7b72e891bcbb883f7bcb8b2e2696d12c83836836
ekalfa2 <74102038+ekalfa2@users.noreply.github.com>
2020-12-27 23:27:47 +0300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name = 'esra'
+surname = 'kalfa'
+lessons=("c1,c2,c3,c4,c5")
+print("[Choice 1: lesson1]")
+print("[Choice 2: lesson2]")
+print("]Choice 3: lesson3]")
+print("[Choice 4: lesson4]")
+print("[Choice 5:lesson5\n]")
+input("You have 3 entrée to log in\n")
+list1=input("Please enter your name")
+list2=input("Please enter your surname")
+while==True
+count=1
+if count<=3:
+    
+if ((list1 == name) and (list2 == surname)):
+ print("Correct... \nWelcome... :)")
+ 
+elif ((list1 != name ) and (list2 == surname)):
+ print("Name is dennied.. Please try again...")
+ 
+elif ((list1 == name) and (list2 != surname)):
+ print("Surname is dennied.. Please try again...")
+ 
+else:
+ print("Please try again...")
+
